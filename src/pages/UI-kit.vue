7151778fca797a0f050c6299165420515dead287
Рафаэль Камашев <wizzzet@gmail.com>
2021-03-01 16:46:28 +0300

    ui kit page blank

---
+++ src/pages/UI-kit.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'UIKitPage'
+})
+</script>
