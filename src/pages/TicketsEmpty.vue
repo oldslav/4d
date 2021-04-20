3f5f5ba1a82d5b67951f85fb18dc4dc50308f70e
oldslav <vyaches.afanas@gmail.com>
2021-04-20 18:24:23 +0400

    task 95 review fixes

--- src/pages/TicketsEmpty.vue
+++ src/pages/TicketsEmpty.vue
@@ -1,35 +1,31 @@
 <template lang="pug">
-  q-page.requests-empty.column.items-center.justify-lg-center.full-height.q-pt-xl.bg-secondary-light
-    h3.requests-empty__title.text-center
+  q-page.tickets-empty.column.items-center.justify-lg-center.full-height.q-pt-xl.bg-secondary-light
+    h4.text-h4.text-center
       | У вас пока нет ни одной заявки
     .text-subtitle.text-center.q-px-lg
       | {{ $t("employee.profile.tickets.empty.subtitle1") }}
       router-link.text-primary(to="/")
-        | {{ $t("employee.profile.tickets.empty.services") }}
+        | {{ $t("entity.services") }}
       | {{ $t("employee.profile.tickets.empty.subtitle2") }}
-    q-img.requests-empty__image.q-mt-xl(src="@/assets/svg/empty-placeholder.svg")
+    q-img.tickets-empty__image.q-mt-xl(src="@/assets/svg/empty-placeholder.svg")
 </template>
 
 <script>
   export default {
-    name: "RequestsEmpty",
+    name: "TicketsEmpty"
   };
 </script>
 
 <style lang="stylus">
-  .requests-empty {
-    &__title {
-      font-size 24px
-    }
-
+  .tickets-empty {
     &__image {
       max-width 650px
     }
   }
 
   @media (max-width: $breakpoint-sm-min) {
-    .requests-empty__title {
-      font-size 20px
+    .tickets-empty__title {
+      font-size $h5
     }
   }
 </style>
