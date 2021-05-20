c313bd94c5e174f583c671b62fdd24a080ba0518
oldslav <vyaches.afanas@gmail.com>
2021-05-20 10:58:09 +0400

    UTI

---
+++ src/pages/tickets/UserTicketsIdeas.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  div
+    base-table(
+      v-if="data"
+      row-key="id"
+      :columns="columns"
+      :data="data"
+      :getData="() => []"
+      :loading="isLoading"
+      :expanded="[]"
+    )
+</template>
+
+<script>
+  import BaseTable from "components/common/BaseTable";
+
+  export default {
+    name: "UserTicketsIdeas",
+    components: { BaseTable },
+    data () {
+      return {
+        data: {}
+      };
+    },
+    computed: {
+      columns () {
+        return [
+          {
+            name: "address",
+            label: "Address",
+            align: "left"
+          },
+          {
+            name: "date",
+            label: "Date",
+            align: "left"
+          },
+          {
+            name: "status",
+            label: "Status",
+            align: "left"
+          }
+        ];
+      },
+      isLoading () {
+        return false;
+      }
+    }
+  };
+</script>
+
+<style lang="stylus"></style>
