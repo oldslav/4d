8498a05207eb3b0e2f6140ad80374886158a7775
oldslav <vyaches.afanas@gmail.com>
2021-06-03 17:55:54 +0400

    dynamic prices for different sizes

--- src/components/services/warehouse/NewTiresTicket.vue
+++ src/components/services/warehouse/NewTiresTicket.vue
@@ -12,6 +12,7 @@
         contracted
         flat
         animated
+        header-class="modal-header"
       )
         q-step(
           title="Основная информация"
@@ -19,19 +20,14 @@
           :name="1"
           icon="edit"
         )
+          .text-medium.q-mb-sm
+            | Основная информация
           .row.q-col-gutter-md
             q-input(v-model="lastname" :label="$t('user.lastName')").col-12.col-sm-6.col-md-4
             q-input(v-model="firstname" :label="$t('user.firstName')").col-12.col-sm-6.col-md-4
             q-input(v-model="patronymic" :label="$t('user.patronymic')").col-12.col-sm-6.col-md-4
           q-separator.q-my-lg
           FilePicker(v-model="passport" :label="$t('entity.files.passportCopy')" :max-files="5")
-          q-separator.q-my-lg.full-width
-          q-option-group(
-            v-model="tireType"
-            color="primary"
-            inline
-            :options="tiresOptions"
-          )
           q-stepper-navigation
             q-btn(@click="step++" color="primary" :label="$t('action.continue')")
         q-step(
@@ -40,6 +36,16 @@
           :name="2"
           icon="edit"
         )
+          .text-medium.q-mb-sm
+            | Радиус колеса
+          q-option-group(
+            v-model="tireType"
+            color="primary"
+            inline
+            :options="tiresOptions"
+            dense
+          ).q-mt-md
+          q-separator.q-my-lg
           .text-medium.q-mb-sm
             | Тип хранения
           .text-small
@@ -53,46 +59,42 @@
               template(v-if="isMobile")
                 q-item-section
                   q-item-label {{ $t("entity.services.warehouse.storagePrice.tires.short.label") }}
-                  q-item-label.text-primary {{ $t("entity.services.warehouse.storagePrice.tires.short.price") }}
+                  q-item-label.text-primary {{ $t("entity.services.warehouse.storagePrice.dynamic", {price: prices.short}) }}
               template(v-else)
                 q-item-section
-                  q-item-label {{ $t('entity.services.warehouse.storagePrice.tires.short.label') }}
+                  q-item-label {{ $t("entity.services.warehouse.storagePrice.tires.short.label") }}
                 q-item-section(side)
-                  q-item-label.text-primary {{ $t('entity.services.warehouse.storagePrice.tires.short.price') }}
+                  q-item-label.text-primary {{ $t("entity.services.warehouse.storagePrice.dynamic", {price: prices.short}) }}
 
             q-item(tag="label" v-ripple).q-px-none
               q-item-section(avatar)
                 q-radio(v-model="storeOption" val="2" dense)
               template(v-if="isMobile")
                 q-item-section
-                  q-item-label {{ $t('entity.services.warehouse.storagePrice.tires.mid.label') }}
+                  q-item-label {{ $t("entity.services.warehouse.storagePrice.tires.mid.label") }}
                   q-item-label(caption) Платеж осуществляется 1 раз в 6 месяцев
-                  q-item-label.text-primary {{ $t('entity.services.warehouse.storagePrice.tires.mid.price') }}
-                  q-item-label(caption) {{ $t('entity.services.warehouse.storagePrice.tires.mid.caption') }}
+                  q-item-label.text-primary {{ $t("entity.services.warehouse.storagePrice.dynamic", {price: prices.mid}) }}
               template(v-else)
                 q-item-section
-                  q-item-label {{ $t('entity.services.warehouse.storagePrice.tires.mid.label') }}
+                  q-item-label {{ $t("entity.services.warehouse.storagePrice.tires.mid.label") }}
                   q-item-label(caption) Платеж осуществляется 1 раз в 6 месяцев
                 q-item-section(side)
-                  q-item-label.text-primary {{ $t('entity.services.warehouse.storagePrice.tires.mid.price') }}
-                  q-item-label(caption) {{ $t('entity.services.warehouse.storagePrice.tires.mid.caption') }}
+                  q-item-label.text-primary {{ $t("entity.services.warehouse.storagePrice.dynamic", {price: prices.mid}) }}
 
             q-item(tag="label" v-ripple).q-px-none
               q-item-section(avatar)
                 q-radio(v-model="storeOption" val="3" dense)
               template(v-if="isMobile")
                 q-item-section
-                  q-item-label {{ $t('entity.services.warehouse.storagePrice.tires.long.label') }}
-                  q-item-label(caption) Платеж осуществляется 1 раз в 11 месяцев
-                  q-item-label.text-primary {{ $t('entity.services.warehouse.storagePrice.tires.long.price') }}
-                  q-item-label(caption) {{ $t('entity.services.warehouse.storagePrice.tires.long.caption') }}
+                  q-item-label {{ $t("entity.services.warehouse.storagePrice.tires.long.label") }}
+                  q-item-label(caption) Платеж осуществляется 1 раз в 12 месяцев
+                  q-item-label.text-primary {{ $t("entity.services.warehouse.storagePrice.dynamic", {price: prices.long}) }}
               template(v-else)
                 q-item-section
-                  q-item-label {{ $t('entity.services.warehouse.storagePrice.tires.long.label') }}
-                  q-item-label(caption) Платеж осуществляется 1 раз в 11 месяцев
+                  q-item-label {{ $t("entity.services.warehouse.storagePrice.tires.long.label") }}
+                  q-item-label(caption) Платеж осуществляется 1 раз в 12 месяцев
                 q-item-section(side)
-                  q-item-label.text-primary {{ $t('entity.services.warehouse.storagePrice.tires.long.price') }}
-                  q-item-label(caption) {{ $t('entity.services.warehouse.storagePrice.tires.long.caption') }}
+                  q-item-label.text-primary {{ $t("entity.services.warehouse.storagePrice.dynamic", {price: prices.long}) }}
 
           q-stepper-navigation.q-gutter-md
             q-btn(@click="step--" color="red" :label="$t('action.back')")
@@ -102,6 +104,8 @@
           :name="3"
           icon="edit"
         )
+          .text-medium.q-mb-sm
+            | Контакты
           FormContacts(v-model="contacts")
           q-stepper-navigation.q-gutter-md
             q-btn(@click="step--" color="red" :label="$t('action.back')")
@@ -129,7 +133,7 @@
         lastname: "",
         patronymic: "",
         passport: null,
-        tireType: null,
+        tireType: 0,
         storeOption: null,
         contacts: {
           phone: null
@@ -137,6 +141,9 @@
       };
     },
     computed: {
+      isMobile () {
+        return this.$q.platform.is.mobile;
+      },
       tiresOptions () {
         return [
           {
@@ -152,6 +159,35 @@
             value: 2
           }
         ];
+      },
+      prices () {
+        if (this.tireType === 1) {
+          return this.mediumPrices;
+        } else if (this.tireType === 2) {
+          return this.largePrices;
+        }
+        return this.smallPrices;
+      },
+      smallPrices () {
+        return {
+          short: 200,
+          mid: 1000,
+          long: 2000
+        };
+      },
+      mediumPrices () {
+        return {
+          short: 250,
+          mid: 1250,
+          long: 2500
+        };
+      },
+      largePrices () {
+        return {
+          short: 300,
+          mid: 1500,
+          long: 3000
+        };
       }
     },
     methods: {
