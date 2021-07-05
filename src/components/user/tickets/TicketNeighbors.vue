068fc8889a67bb5b63c72774cdf7b3b0718ae5cc
oldslav <vyaches.afanas@gmail.com>
2021-07-05 11:45:39 +0400

    TicketNeighbors.vue

---
+++ src/components/user/tickets/TicketNeighbors.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+
+</template>
+
+<script>
+  import NeighborResolver from "components/user/documents/NeighborResolver";
+
+  const defaultNeighbor = (id) => ({
+    name: {
+      last: null,
+      full: null,
+      first: null,
+      noPatronymic: false,
+      patronymic: null
+    },
+    neighborTypeId: id,
+    documents: {
+      inn: [],
+      snils: [],
+      passport: [],
+      marriage: [],
+      birth: [],
+      children_registration: [],
+      consent_processing_personal_data: []
+    }
+  });
+
+  export default {
+    name: "TicketNeighbors",
+    components: { NeighborResolver },
+    props: {
+      value: {
+        type: Array,
+        default: () => []
+      }
+    },
+    data () {
+      return {
+        model: []
+      };
+    },
+    methods: {
+      addNewNeighbor (id) {
+        this.model.push(defaultNeighbor(id));
+      }
+    }
+  };
+</script>
