c2ff8e0dcd9af076ea617313ad1b71885a385f04
i.bosykh@innopolis.ru <9JKpE3XQ!PdXBwB>
2021-08-24 13:53:18 +0300

    359

--- src/pages/services/ServiceIdeas.vue
+++ src/pages/services/ServiceIdeas.vue
@@ -1,22 +1,51 @@
 <template lang="pug">
-  div
-    q-btn(label="asdad" color="primary")
-    NewIdeaModal(v-if="pointCoords" v-model="pointCoords")
+  .q-pa-lg(style="height: calc(100vh - 50px)")
+    .col-12.column.justify-between.full-height(v-if="componentInstance")
+      .row
+        IdeaDetailsCard(v-if="featureId" :id="featureId")
+        q-inner-loading(:showing="isLoading")
+      .text-center
+        q-btn(color="primary" label="Создать заявку" @click="setDrawType('pointPrimitive')")
+    NewIdeaModal(v-if="pointCoords" v-model="pointCoords" @created="onIdeaCreated")
 </template>
 
 <script>
-  import { mapActions, mapMutations } from "vuex";
-  import { GET_IDEAS_GEO } from "../../store/constants/action-constants";
+  import { mapActions, mapMutations, mapState } from "vuex";
+  import { GET_DATA, GET_IDEAS_GEO, UPDATE_LIKE } from "../../store/constants/action-constants";
   import NewIdeaModal from "../../components/services/ideas/NewIdeaModal";
-  import { SET_POINT_COORDS } from "../../store/constants/mutation-constants";
+  import { SET_DRAW_TYPE, SET_POINT_COORDS } from "../../store/constants/mutation-constants";
+  import IdeaDetailsCard from "../../components/services/ideas/IdeaDetailsCard";
 
   export default {
     name: "ServiceIdeas",
-    components: { NewIdeaModal },
-    async created () {
-      await this.GET_IDEAS_GEO();
+    components: { NewIdeaModal, IdeaDetailsCard },
+    props: {
+      map: {
+        type: [Object, null],
+        default: null
+      }
+    },
+    data () {
+      return {
+        componentInstance: null
+      };
     },
     computed: {
+      ...mapState("services", {
+        geoJson: state => state.geoJson,
+        featureId: state => state.pickedFeatureId,
+        cesiumInstance: state => state.cesiumInstance,
+        isDraw: state => state.isDraw
+      }),
+
+      isLoading () {
+        return this.$store.state.wait[`services/ideas/${ GET_DATA }`];
+      },
+
+      authorName () {
+        return this.current.author.name.full || `${ this.current.author.name.first } ${ this.current.author.name.last }`;
+      },
+
       pointCoords: {
         get () {
           return Boolean(this.$store.state.services.pointCoords);
@@ -24,6 +53,7 @@
 
         set () {
           this.SET_POINT_COORDS(null);
+          this.setDrawType(null);
         }
       }
     },
@@ -32,9 +62,42 @@
         GET_IDEAS_GEO
       ]),
 
-      ...mapMutations("services", [
-        SET_POINT_COORDS
-      ])
+      ...mapActions("services/ideas", [
+        GET_DATA,
+        UPDATE_LIKE
+      ]),
+
+      ...mapMutations("services", {
+        SET_POINT_COORDS,
+        setDrawType: SET_DRAW_TYPE
+      }),
+
+      async onIdeaCreated () {
+        await this.GET_IDEAS_GEO();
+        this.$q.notify({
+          type: "positive",
+          message: "Идея успешно создана"
+        });
+      },
+
+      async onViewerReady () {
+        await this.GET_IDEAS_GEO();
+      }
+    },
+    watch: {
+      cesiumInstance: {
+        immediate: true,
+        handler (value) {
+          if (value) {
+            this.componentInstance = this.$root.map.componentInstance;
+            this.onViewerReady();
+          }
+        }
+      },
+
+      async featureId (val) {
+        if (val) await this.GET_DATA(val);
+      }
     }
   };
 </script>
