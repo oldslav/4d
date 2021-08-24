c8d5a494d5c0b7311a8f995de7d35278ff13916c
Vladislav Donskikh <v.donskikh@innopolis.ru>
2021-08-24 15:29:08 +0300

    #758 фикс степов заявок парковок сотрудника фонда

--- src/pages/tickets/EmployeeTicketsParking.vue
+++ src/pages/tickets/EmployeeTicketsParking.vue
@@ -14,7 +14,7 @@
         q-tr(:props="props"  @click="expandRow(props)")
           q-td(key="fullname" :props="props")
             | {{props.row.name.full}}
-          q-td(key="parkingAddress" :props="props")
+          q-td(key="parkingAddress" :props="props") 
             | {{props.row.parkingPlace.address}}
           q-td(key="parkingNumber" :props="props")
             | {{props.row.parkingPlace.number}}
@@ -38,39 +38,47 @@
 
         q-tr.step-details(v-show="props.expand" :props="props")
           q-td(colspan="100%").is-paddingless
+            div.column(v-if="props.row.status.id === 2").q-pa-md
+              div.text-body1.text-wrap
+                | Примите или отклоните заявку
+            div.column(v-if="props.row.status.id === 6").q-pa-md
+              div.text-right.text-body1.text-wrap
+                | Для подписание договора направьте заявителю приглашение.<br>
+                | Вы можете изменить шаблон сообщения по вашему желанию.
+            div.column(v-if="props.row.status.id === 8").q-pa-md
+              div.text-body1.text-wrap
+                | Договор подписан
+            div.column(v-if="[4, 9].includes(props.row.status.id)").q-pa-md
+              div.text-body1.text-wrap
+                | Работа над заявкой завершена
             q-stepper(
               ref="stepper"
               :value="props.row.status.id"
               color="primary"
               flat
               animated
+              v-if="props.row.status.id > 2 && props.row.status.id < 8 && props.row.status.id !== 4"
             )
               q-step(
                 :title="$t('user.tickets.parking.steps.first')"
                 :done="props.row.status.id > 4"
                 :name="3"
               )
+                div.text-right.text-body1.text-wrap
+                  | Для продолжения оформления документов дождитесь оплаты.
               q-step(
                 :title="$t('user.tickets.parking.steps.second')"
                 :done="props.row.status.id > 7"
                 :name="7"
               )
-            div(v-if="props.row.status.id === 3").q-pa-md
-              div.text-right.text-body1.text-wrap
-                | Для продолжения оформления документов дождитесь оплаты.
-            div(v-if="props.row.status.id === 6").q-pa-md
-              div.text-right.text-body1.text-wrap
-                | Для подписание договора направьте заявителю приглашение.<br>
-                | Вы можете изменить шаблон сообщения по вашему желанию.
-            div(v-if="props.row.status.id === 7").q-pa-md
-              .row
-                .col-6.offset-6
-                  .text-body1.q-col-gutter-md
-                    div
-                      | Договор подписан.
-                    div
-                      | Введите данные договора.
-                    FormContract(@submit="sendContractInfo($event, props.row.id)")
+                .row
+                  .col-6.offset-6
+                    .text-body1.q-col-gutter-md
+                      div
+                        | Договор подписан.
+                      div
+                        | Введите данные договора.
+                      FormContract(@submit="sendContractInfo($event, props.row.id)")
 
     TicketDetailsModal(v-model="showDetailsModal" :info="activeRow" @reject="onTicketReject" @approve="onTicketApprove")
 </template>
@@ -293,7 +301,7 @@
   };
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
 .step-details
   background-color: #DEEFFE
 .q-stepper
