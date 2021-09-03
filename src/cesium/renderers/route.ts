34700742c2bfdf376dda9e2592b7652d534746cc
Petr Chalov <tchalov.pyotr@yandex.ru>
2021-09-03 18:02:27 +0400

    fixes, visible for section

--- src/cesium/renderers/route.ts
+++ src/cesium/renderers/route.ts
@@ -3,7 +3,7 @@ import { updateEntity } from "src/cesium/utils/update-entity";
 export default {
   name: "route",
   render (entity: ICesiumEntity): void {
-    const visible = entity.properties.$visible && entity.properties.$visible.getValue<boolean>();
+    const visible = entity.properties.$visible ? entity.properties.$visible.getValue<boolean>() : true;
     const isExtended = entity.properties.$extended ? entity.properties.$extended.getValue<boolean>() : null;
     const fill = entity.properties.fill ? entity.properties.fill.getValue<string>() : null;
     const opacity = entity.properties["stroke-opacity"] ? entity.properties["stroke-opacity"].getValue<number>() : 0.5;
